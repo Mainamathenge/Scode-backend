354b69cb4a362d7e98492456c53b30f087fc8c24
Joseph Maina <99658350+Mainamathenge@users.noreply.github.com>
2024-03-28 22:33:43 +0300

    added-message-callbacl

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -35,6 +35,6 @@ exports.updateDevice = catchAsync(async (req, res, next) => {
   console.log("update device", req.body);
   res.status(200).json({
     status: "success",
-    data: { message: "Device updated" },
+    data: req.body,
   });
 });
